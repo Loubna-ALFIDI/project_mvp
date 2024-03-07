de21d766b33ca849c0f1b518cb4153884a59c95c
HichamLuffy <imalexvssteve2020@gmail.com>
2024-03-07 18:41:48 +0100

    profile page

--- project_mvp/Aquiz/static/scripts/editprofile.js
+++ project_mvp/Aquiz/static/scripts/editprofile.js
@@ -1,23 +1,13 @@
 $(document).ready(function() {
     $('#edit-profile-link').click(function(event) {
         event.preventDefault();
+        $('#profile-details').toggle();
         $('#edit-profile-div').toggle();
     
         if ($('#edit-profile-div').is(':visible')) {
+            $('profile-details').hide();
             // Populate the edit fields with current user's details
-            $('#username-edit').val('{{ current_user.username }}');
-            $('#email-edit').val('{{ current_user.email }}');
-            // Show the edit fields
-            $('#username-edit').show();
-            $('#email-edit').show();
-            // Show the update button
-            $('#update-profile-button').show();  // Assuming this is the ID of your update button
-        } else {
-            // Hide the edit fields if the edit profile div is hidden
-            $('#username-edit').hide();
-            $('#email-edit').hide();
-            // Hide the update button
-            $('#update-profile-button').hide();  // Assuming this is the ID of your update button
+            // Assuming this is the ID of your update button
         }
     });
 });
